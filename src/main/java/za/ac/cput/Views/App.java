60998c85a5b9447af95e7e755ca49c18ea4a27d7
Keenan <Keenansolomons18@gmail.com>
2021-10-20 23:51:19 +0200

    readded sout

--- src/main/java/za/ac/cput/Views/App.java
+++ src/main/java/za/ac/cput/Views/App.java
@@ -1,14 +1,6 @@
 package za.ac.cput.Views;
 
-import com.google.gson.Gson;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import za.ac.cput.Entity.Course;
 
-import java.io.IOException;
 
 /**
  * Hello world!
